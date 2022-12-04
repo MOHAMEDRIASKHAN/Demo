d5a606a86e754496622ceedb028e9e3737f77eaf
MOHAMED RIASKHAN <mohamedriaskhan@gmail.com>
2022-12-04 11:41:13 +0530

    Add

---
+++ DemoProject/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace DemoProject
+{
+    class program
+    {
+        public string void Main(string[] args)
+        {
+            
+            int a = 20;
+            int b = 30;
+
+            int add = a + b;
+        }
+    }
+}
